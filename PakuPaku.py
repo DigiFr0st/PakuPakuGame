dbfab0a65f08ce15dd103b3f103a4cb35e9e8fe4
DigiFr0st <digifrost.business@gmail.com>
2023-07-02 08:44:52 -0400

    Town map added

--- PakuPaku.py
+++ PakuPaku.py
@@ -2,39 +2,48 @@ import pygame
 
 # pygame setup pieces
 pygame.init()
-display = pygame.display.set_mode((800, 600))
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 1024
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("PakuDex")
 clock = pygame.time.Clock()
-gameRunning = True
+game_Running = True
 dt = 0
 
-playerPos = pygame.Vector2(display.get_width() / 2, display.get_height() / 2)
+player_Pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+ground_surface = pygame.image.load("maps/town_1_1.png")
+map_width = ground_surface.get_width()
+map_height = ground_surface.get_height()
+scaled_GS = pygame.transform.scale(ground_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-while gameRunning:
+while game_Running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            gameRunning = False
+            game_Running = False
 
     # fill the game screen to remove anything from the last frame
-    display.fill("purple")
+    screen.fill("black")
 
     # render game here
-    pygame.draw.circle(display, "red", playerPos, 40)
+    screen.blit(scaled_GS, (0, 0))
+    player = pygame.draw.circle(screen, "red", player_Pos, 10)
+
+    # screen.blit(red_surface, (200, 100))
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        playerPos.y -= 300 * dt
+        player_Pos.y -= 300 * dt
     if keys[pygame.K_s]:
-        playerPos.y += 300 * dt
+        player_Pos.y += 300 * dt
     if keys[pygame.K_a]:
-        playerPos.x -= 300 * dt
+        player_Pos.x -= 300 * dt
     if keys[pygame.K_d]:
-        playerPos.x += 300 * dt
+        player_Pos.x += 300 * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(60) / 2000
 
 pygame.quit()
 exit()

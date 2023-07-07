ea4a2c5c30aa5a1b92cb9c90e315cc9249f17a7c
DigiFr0st <digifrost.business@gmail.com>
2023-07-06 21:40:52 -0400

    player sprite added
    Replace the annoying red dot with a single sprite. Also added a textbox but will remove later

--- PakuPaku.py
+++ PakuPaku.py
@@ -1,20 +1,53 @@
 import pygame
 
-# pygame setup pieces
 pygame.init()
+
+# constants
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 1024
+BACKGROUND_COLOR = "black"
+
+# initalization pieces
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("PakuDex")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
 game_Running = True
 dt = 0
-
 player_Pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-ground_surface = pygame.image.load("maps/town_1_1.png")
-map_width = ground_surface.get_width()
-map_height = ground_surface.get_height()
-scaled_GS = pygame.transform.scale(ground_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# images for game
+ground_image = pygame.image.load("maps/town_1_1.png")
+# building_foreground_image = pygame.image.load("maps/building_foreground_layer.png")
+player_sprite_sheet_image = pygame.image.load("sprites/character.png").convert_alpha()
+
+# surfaces for game
+ground_surface = pygame.transform.scale(ground_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+text_surface = test_font.render("Paku-Paku", False, "white")
+
+
+def get_player_image_scaled(
+    sheet,
+    color_key,
+    scale,
+    player_width,
+    player_height,
+    sprite_x1,
+    sprite_y1,
+    sprite_x2,
+    sprite_y2,
+):
+    image = pygame.Surface((player_width, player_height)).convert_alpha()
+    image.blit(sheet, (0, 0), (sprite_x1, sprite_y1, sprite_x2, sprite_y2))
+    image = pygame.transform.scale(image, (player_width * scale, player_height * scale))
+    image.set_colorkey(color_key)
+    return image
+
+
+frame_0 = get_player_image_scaled(
+    player_sprite_sheet_image, BACKGROUND_COLOR, 2, 16, 32, 0, 0, 16, 32
+)
+
 
 while game_Running:
     for event in pygame.event.get():
@@ -22,13 +55,13 @@ while game_Running:
             game_Running = False
 
     # fill the game screen to remove anything from the last frame
-    screen.fill("black")
+    screen.fill(BACKGROUND_COLOR)
 
     # render game here
-    screen.blit(scaled_GS, (0, 0))
-    player = pygame.draw.circle(screen, "red", player_Pos, 10)
-
-    # screen.blit(red_surface, (200, 100))
+    screen.blit(ground_surface, (0, 0))
+    screen.blit(text_surface, (580, 0))
+    # player = pygame.draw.circle(screen, "red", player_Pos, 10)
+    screen.blit(frame_0, player_Pos)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -43,7 +76,7 @@ while game_Running:
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    dt = clock.tick(60) / 2000
+    dt = clock.tick(60) / 1500
 
 pygame.quit()
 exit()
